be38ffe9ac981450c3d16b1e3b765618aed85ca3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-07 21:00:44 +0600

    event code added

---
+++ Module-25/script.js
@@ -0,0 +1,9 @@
+function backgroundRed() {
+    document.body.style.backgroundColor = 'red'
+}
+function backgroundGreen() {
+    document.body.style.backgroundColor = 'green'
+}
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}

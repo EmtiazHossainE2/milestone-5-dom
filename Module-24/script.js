818725b4cd596eb02c07735cfaa9e29a5f5bc891
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-07 13:01:36 +0600

    query selector style

--- Module-24/script.js
+++ Module-24/script.js
@@ -26,17 +26,12 @@ blog2.style.color = 'red'
 blog2.style.fontSize = '20px'
 blog2.style.fontWeight = 'blod'
 
-// explore getElementByClassName 
-const blogs = document.getElementsByClassName('blog')
-for (const blog of blogs) {
-    blog.style.border = '2px solid orange'
-    blog.style.borderRadius = '10px'
-    blog.style.margin = '6px'
-    blog.style.padding = '10px'
-}
 
-/* const query = document.querySelectorAll('.blog p')
-    .blog p.style.color = 'green' */
+
+const query = document.querySelectorAll('.blog p')
+for (const num of query) {
+    num.style.color = 'green'
+}
 
 // get and add element using js 
 //create element 
@@ -47,4 +42,35 @@ li.innerText = 'blog-5';
 const ul = document.getElementById('blog-list')
 
 //append new element 
-ul.appendChild(li) 
+ul.appendChild(li)
+
+// add article 
+const article = document.createElement('article')
+article.classList.add('blog')
+// manually add  
+/* const h2 = document.createElement('h2')
+h2.innerText = 'My-Awesome-Blog-4'
+const p = document.createElement('p')
+p.innerText = 'ipsum dolor sit amet consectetur adipisicing elit. Error quisquam necessitatibus quidem aute';
+article.appendChild(h2);
+article.appendChild(p); */
+
+// innerHTML 
+article.innerHTML = `
+    <h2>My-Awesome-Blog-4 </h2>
+    <p>ipsum dolor sit amet consectetur adipisicing elit. Error quisquam necessitatibus quidem aute</p>
+`
+
+const blogsSection = document.getElementById('blogs')
+blogsSection.appendChild(article)
+
+
+
+// explore getElementByClassName 
+const blogs = document.getElementsByClassName('blog')
+for (const blog of blogs) {
+    blog.style.border = '2px solid orange'
+    blog.style.borderRadius = '10px'
+    blog.style.margin = '6px'
+    blog.style.padding = '10px'
+}

b4974735bbc752ed1b3f289dfe8f65fd7917cbc1
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-08 12:28:09 +0600

    focus blur keyup change js

---
+++ Module-25/delete.js
@@ -0,0 +1,28 @@
+document.getElementById('delete-btn').addEventListener('click', function () {
+    document.getElementById('delete-text').style.display = 'none'
+})
+//focus
+document.getElementById('delete-confirm').addEventListener('focus', function () {
+    document.body.style.backgroundColor = 'red'
+});
+//blur
+document.getElementById('delete-confirm').addEventListener('blur', function () {
+    document.body.style.backgroundColor = 'white'
+});
+//keyup 
+document.getElementById('delete-confirm').addEventListener('keyup', function (event) {
+    /* const deleteConfirm = document.getElementById('delete-confirm');
+    console.log(deleteConfirm.value); */
+    let deleted = document.getElementById('delete-btn');
+    if (event.target.value == 'delete') {
+        deleted.removeAttribute('disabled')
+    }
+    else {
+        deleted.setAttribute('disabled', true)
+    }
+})
+//change 
+document.getElementById('delete-confirm').addEventListener('change', function () {
+    const deleteConfirm = document.getElementById('delete-confirm');
+    console.log(deleteConfirm.value);
+})

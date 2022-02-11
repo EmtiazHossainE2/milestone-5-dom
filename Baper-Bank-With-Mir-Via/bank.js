6fe5024632fb8387f35680149877149e48701c7c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-12 04:17:15 +0600

    Common function to reduce code repeat

--- Baper-Bank-With-Mir-Via/bank.js
+++ Baper-Bank-With-Mir-Via/bank.js
@@ -1,32 +1,40 @@
-/* *****   ----   potibar console log kora valo  ----  ***
-1.jei btn ke click korbo oi btn ke onclick diye dhore niye asci .
-2.input value ta lagbe tai id diye nici . 
-3.value set korte hobe jonne depositoutput e id diye niye asci . eta html tag jonne inner text diye dhorte hobe . 
-4. depositOutput er innerText ke  = depositValue.value set kore dici . 
-5. value 2 ta adition korar jonne depositOutput er innerText e = parsefloat korci depositOutput.innerText & depositValue.value ke . 
-6. sesh e deposit value ke empty korci 
+//deposit 
+let depositValue = document.getElementById('deposit-value');
+let depositOutput = document.getElementById('deposit-output');
 
-*/
+//withdraw 
+let withdrawValue = document.getElementById('withdraw-value');
+let withdrawOutput = document.getElementById('withdraw-output');
+
+//total balance
+let balanceOutput = document.getElementById('balance-output');
+
+//for addition 
+function addMoney(currentAmount, inputAmount) {
+    return parseFloat(currentAmount) + parseFloat(inputAmount)
+}
+//for subtraction 
+function getMoney(currentAmount, inputAmount) {
+    return parseFloat(currentAmount) - parseFloat(inputAmount)
+}
 
 function deposit() {
-    let depositValue = document.getElementById('deposit-value');
-    let depositOutput = document.getElementById('deposit-output');
-    let balanceOutput = document.getElementById('balance-output');
+    const totalDeposit = addMoney(depositOutput.innerText, depositValue.value)
     //deposit output
-    depositOutput.innerText = parseFloat(depositOutput.innerText) + parseFloat(depositValue.value);
+    depositOutput.innerText = totalDeposit;
     //balance output
-    balanceOutput.innerText = parseFloat(balanceOutput.innerText) + parseFloat(depositValue.value)
+    const totalBalance = addMoney(balanceOutput.innerText, depositValue.value)
+    balanceOutput.innerText = totalBalance;
     depositValue.value = '';
 }
 
 function withdraw() {
-    let withdrawValue = document.getElementById('withdraw-value');
-    let withdrawOutput = document.getElementById('withdraw-output');
-    let balanceOutput = document.getElementById('balance-output');
+    const totalWithdraw = addMoney(withdrawOutput.innerText, withdrawValue.value)
     //withdrawoutput
-    withdrawOutput.innerText = parseFloat(withdrawOutput.innerText) + parseFloat(withdrawValue.value);
+    withdrawOutput.innerText = totalWithdraw;
     //balance output
-    balanceOutput.innerText = parseFloat(balanceOutput.innerText) - parseFloat(withdrawValue.value)
+    const totalBalance = getMoney(balanceOutput.innerText, withdrawValue.value)
+    balanceOutput.innerText = totalBalance;
 
     withdrawValue.value = '';
 }

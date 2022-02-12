d83a13ab3418634ccd6e49d5c63a83a676f109f9
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-12 15:44:10 +0600

    complete without error handling

--- Mayer Doya Bap Er Bank Recap/bank.js
+++ Mayer Doya Bap Er Bank Recap/bank.js
@@ -1,1 +1,25 @@
-console.log('working');
+//deposit calculation 
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const depositValue = document.getElementById('deposit-value');
+    const depositOutput = document.getElementById('deposit-output');
+    depositOutput.innerText = parseFloat(depositOutput.innerText) + parseFloat(depositValue.value);
+    //balance output calculation
+    const balanceOutput = document.getElementById('balance-output');
+    balanceOutput.innerText = parseFloat(balanceOutput.innerText) + parseFloat(depositValue.value);
+
+    depositValue.value = '';
+})
+
+// withdraw calculation 
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawValue = document.getElementById('withdraw-value');
+    const withdrawOutput = document.getElementById('withdraw-output');
+    withdrawOutput.innerText = parseFloat(withdrawOutput.innerText) + parseFloat(withdrawValue.value);
+
+    //balance output calculation 
+    const balanceOutput = document.getElementById('balance-output');
+    balanceOutput.innerText = parseFloat(balanceOutput.innerText) - parseFloat(withdrawOutput.innerText);
+
+    withdrawValue.value = ''
+
+})

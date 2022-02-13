f1e093e2a2f1d1f74c877794fb328cbf13ba238e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-13 13:04:32 +0600

    case total calculation

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,14 +1,24 @@
 function updateCaseNumber(isIncreasing) {
     const caseValue = document.getElementById('case-value');
-    const caseNumber = caseValue.value;
+    let caseNumber = caseValue.value;
     if (isIncreasing) {
-        caseValue.value = parseInt(caseNumber) + 1;
+        caseNumber = parseInt(caseNumber) + 1;
     }
     else if (caseNumber > 0) {
-        caseValue.value = parseInt(caseNumber) - 1;
+        caseNumber = parseInt(caseNumber) - 1;
     }
-}
+    caseValue.value = caseNumber
+    //case total 
+    const caseTotal = document.getElementById('case-total')
+    caseTotal.innerText = caseNumber * 59
 
+}
+// phone increase decrease event 
+document.getElementById('phone-plus').addEventListener('click', function () {
+    console.log('clicked');
+})
+
+//case total increase decrease event
 document.getElementById('case-plus').addEventListener('click', function () {
     updateCaseNumber(true)
 })

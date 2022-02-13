15575912c74ea76e4becca8e92e4b1441f55f96e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-13 23:38:36 +0600

    pin match check

--- pin-mathcer/script.js
+++ pin-mathcer/script.js
@@ -13,3 +13,35 @@ function generateBtn() {
     const pin = getPin()
     document.getElementById('show-pin').value = pin
 }
+
+//calculator body 
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = ''
+        }
+        else if (number == '<') {
+            calcInput.value = '-'
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number
+        calcInput.value = newNumber;
+    }
+})
+
+//verify pin 
+document.getElementById('verify-btn').addEventListener('click', function () {
+    const showPin = document.getElementById('show-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    if (showPin == typedNumbers) {
+        console.log('matched');
+        return
+    }
+    else {
+        console.log('dont match')
+    }
+})

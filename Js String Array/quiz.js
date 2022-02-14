12a6c64ab09c7bb5f6a41486166cda02da871df9
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-14 21:28:39 +0600

    quiz solve

---
+++ Js String Array/quiz.js
@@ -0,0 +1,15 @@
+// 2
+const task = "Practice Makes a person perfect";
+const index = task.toLowerCase().indexOf('M');
+console.log(index)
+
+
+// 12
+let str12 = 'abul'
+let str22 = 'babul'
+let add = str22 + str12
+let add2 = str22.concat(str12)
+
+console.log(add)
+console.log(add2)
+

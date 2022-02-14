7864452632a259e1bb6b6c4fad75e5a8c80c5d09
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-14 19:07:12 +0600

    case convert js

--- Js String Array/case-convert.js
+++ Js String Array/case-convert.js
@@ -1,6 +1,10 @@
-const userInput = 'EmtiazE2';
-const saveUserName = 'emtiaze2';
-if (userInput.toLowerCase() == saveUserName.toLowerCase()) {
+const userEmail = 'emtiazHossainRzs@gmail.Com';
+const saveUserEmail = 'emtiazhossainrzs@gmail.com';
+/* const abc = userEmail.toLowerCase();
+console.log(abc)
+const abc2 = saveUserEmail.toLowerCase()
+console.log(abc2) */
+if (userEmail.toLowerCase() == saveUserEmail.toLowerCase()) {
     console.log('user exists')
 }
 console.log('test okay')

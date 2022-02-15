66bf0c62155d079bbe78b845c936db8fe3b3ef13
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-15 15:12:21 +0600

    update product total price

--- recap-shopping-cart/app.js
+++ recap-shopping-cart/app.js
@@ -1,29 +1,34 @@
 //common function 
-function getValue(product, isIncrease) {
+function getValue(product, isIncrease, price) {
     const productInput = document.getElementById(product + '-value');
     let productValue = productInput.value;
     if (isIncrease) {
-        productInput.value = parseInt(productValue) + 1;
+        productValue = parseInt(productValue) + 1;
     }
     else if (productValue > 0) {
-        productInput.value = parseInt(productValue) - 1;
+        productValue = parseInt(productValue) - 1;
     }
+    productInput.value = productValue;
+
+    //update product total 
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productValue * price
 }
 //phone plus
 document.getElementById('phone-plus').addEventListener('click', function () {
-    getValue('phone', true)
+    getValue('phone', true, 1500)
 })
 
 //phone-minus 
 document.getElementById('phone-minus').addEventListener('click', function () {
-    getValue('phone', false)
+    getValue('phone', false, 1500)
 })
 //case plus
 document.getElementById('case-plus').addEventListener('click', function () {
-    getValue('case', true)
+    getValue('case', true, 100)
 })
 
 //case-minus 
 document.getElementById('case-minus').addEventListener('click', function () {
-    getValue('case', false)
+    getValue('case', false, 100)
 })

5f03f7eba05e9fbcb7f90256892274f1e97f6164
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-15 16:15:07 +0600

    checkout page added

--- recap-shopping-cart/app.js
+++ recap-shopping-cart/app.js
@@ -12,7 +12,10 @@ function getValue(product, isIncrease, price) {
 
     //update product total 
     const productTotal = document.getElementById(product + '-total');
-    productTotal.innerText = productValue * price
+    productTotal.innerText = productValue * price;
+
+    //update balance 
+    updateBalance()
 }
 //phone plus
 document.getElementById('phone-plus').addEventListener('click', function () {
@@ -32,3 +35,26 @@ document.getElementById('case-plus').addEventListener('click', function () {
 document.getElementById('case-minus').addEventListener('click', function () {
     getValue('case', false, 100)
 })
+
+//calculate balance (common function )
+function calculatetotal(product) {
+    const productInput = document.getElementById(product + '-value')
+    const productValue = parseInt(productInput.value);
+    return productValue;
+}
+
+//update balance  
+function updateBalance() {
+    const phoneTotal = calculatetotal('phone') * 1500;
+    const caseTotal = calculatetotal('case') * 100;
+    const subtotal = phoneTotal + caseTotal;
+    const tax = subtotal / 10;
+    const total = subtotal + tax
+    document.getElementById('sub-total').innerText = subtotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = total;
+}
+
+function checkOut() {
+    window.location.href = 'checkOut.html';
+}
